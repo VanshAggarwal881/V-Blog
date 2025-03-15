6309fcfd1a4051b2005b588026c5018749fc6312
Vansh Aggarwal <vanshaggarwal881@gmail.com>
2025-03-15 15:59:38 +0530

    add AuthLayout, Login, and SignUp components with authentication handling

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,21 +1,3 @@
-/*
-MAKING OF A LOGIN COMPONENT WHEN USING ANY BACKEND AS A SERVICE AND STATE MANAGEMENT TOOL LIKE RTK.
-* 1. Link : to take user somewhere after clicking
-* 2. Navigate : for safety , take both
-TODO: 3.import login from auth slice ..... for using in different components
- * 3. use useDispatch : tell Redux to update the state accordingly.
-* 4. authService from appwrite : 
-? This is the actual login engine — the thing that connects to your backend/auth provider.
-! Example:
-Let’s say 
-authService.loginUser(email, password) 
-talks to Appwrite's authentication API and 
-! returns 
-the logged-in user session.
-
-* 5. Last : useForm as we are using React-Hook-Form ofcourse
-*/
-
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { login as storeLogin } from "../store/authSlice";
@@ -24,34 +6,28 @@ import authService from "../appwrite/auth";
 import { useForm } from "react-hook-form";
 import Input from "./Input";
 
-function Login() {
+function SignUp() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const { register, handleSubmit } = useForm();
   const [error, seterror] = useState("");
 
-  const login = async (data) => {
+  const create = async () => {
     seterror("");
     try {
-      // auth service returs session
-      const session = await authService.login(data);
-      if (session) {
-        // again using powers of authService
-        const userData = await authService.getCurrentUser();
-        /*
-            ! ICE BREAKING MOMENT : userdata is the data of the posts and blogs and blogs have to be displayed and displaying of blog will lead to change in state , so state management tool RTK comes in picture here : useDispatch time
-             */
-        if (userData) {
-          dispatch(storeLogin(userData));
-        }
+      const userData = await authService.createAccount(data);
+      if (userData) {
+        const currentUserData = authService.getCurrentUser();
+        if (currentUserData) dispatch(storeLogin(currentUserData));
         navigate("/");
       }
     } catch (error) {
       seterror(error.message);
     }
   };
+
   return (
-    <div className="flex items-center justify-center w-full">
+    <div className="flex items-center justify-center">
       <div
         className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}
       >
@@ -59,22 +35,30 @@ function Login() {
                     <span className="inline-block w-full max-w-[100px]">
                         <Logo width="100%" />
                     </span>
-        </div> */}
+            </div> */}
         <h2 className="text-center text-2xl font-bold leading-tight">
-          Sign in to your account
+          Sign up to create account
         </h2>
         <p className="mt-2 text-center text-base text-black/60">
-          Don&apos;t have any account?&nbsp;
+          Already have an account?&nbsp;
           <Link
-            to="/signup"
+            to="/login"
             className="font-medium text-primary transition-all duration-200 hover:underline"
           >
-            Sign Up
+            Sign In
           </Link>
         </p>
         {error && <p className="text-red-600 mt-8 text-center">{error}</p>}
-        <form onSubmit={handleSubmit(login)} className="mt-8">
+
+        <form onSubmit={handleSubmit(create)}>
           <div className="space-y-5">
+            <Input
+              label="Full Name: "
+              placeholder="Enter your full name"
+              {...register("name", {
+                required: true,
+              })}
+            />
             <Input
               label="Email: "
               placeholder="Enter your email"
@@ -97,7 +81,9 @@ function Login() {
                 required: true,
               })}
             />
-            <button className="btn btn-outline btn-primary">Sign In</button>
+            <button type="submit" className="btn btn-outline btn-primary">
+              Sign Up
+            </button>
           </div>
         </form>
       </div>
@@ -105,4 +91,4 @@ function Login() {
   );
 }
 
-export default Login;
+export default SignUp;
