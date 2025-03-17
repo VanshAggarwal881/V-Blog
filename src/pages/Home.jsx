cc8d5b82c6dc008e4cb104a19b3ba54e7c08e353
Vansh Aggarwal <vanshaggarwal881@gmail.com>
2025-03-17 09:27:27 +0530

    add Home and Post pages with post retrieval and management functionality

---
+++ src/pages/Home.jsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import service from "../appwrite/crud";
+import PostCard from "../components/PostCard.jsx";
+
+function Home() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    service.getPosts().then((posts) => {
+      if (posts) {
+        setPosts(posts.documents);
+      }
+    });
+  });
+  if (posts.length === 0) {
+    return (
+      <div className="w-full py-8 mt-4 text-center">
+        {/* this class can cause problems , be aware */}
+        <div className="w-full max-w-7xl mx-auto px-4">
+          <h1 className="text-2xl font-bold hover:text-gray-500">
+            Login to read posts
+          </h1>
+        </div>
+      </div>
+    );
+  }
+  return (
+    <div className="w-full py-8">
+      <div className="w-full max-w-7xl mx-auto px-4">
+        <div className="flex flex-wrap">
+          {posts.map((post) => (
+            <div key={post.$id} className="p-2 w-1/4">
+              <PostCard {...post} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
